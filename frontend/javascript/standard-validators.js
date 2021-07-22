5d8ba718e829e082102fd89480ea1c55ac82174b
Christian Aistleitner <christian@quelltextlich.at>
2021-07-22 08:48:43 +0200

    standard-validators: Split off checks from validators
    This will make re-use simpler.

--- frontend/javascript/standard-validators.js
+++ frontend/javascript/standard-validators.js
@@ -2,43 +2,83 @@
 // GNU Affero General Public License v3.0 (See LICENSE.md)
 // SPDX-License-Identifier: AGPL-3.0-only
 
-function standard_not_empty_validator(node) {
-    const password = node.value;
+// -- Standard checks --------------------------------------------------
 
-    if (password.length == '') {
+function standard_check_not_empty(value) {
+    if (value == '') {
         return localize('Must not be empty');
     }
 
     return true;
 }
 
+function standard_check_minimal_length(value, minimal_length) {
+    if (value.length < minimal_length) {
+        return localize('Too short (minimal length: {minimal_length})', {'minimal_length': String(minimal_length)});
+    }
+    return true;
+}
+
+function standard_check_maximal_length(value, maximal_length) {
+    if (value.length > maximal_length) {
+        return localize('Too long (maximum length: {maximum_length})', {'maximum_length': String(maximal_length)});
+    }
+    return true;
+}
+
+function standard_check_does_not_start_with(value, forbidden_start) {
+    if (value && value[0] == forbidden_start) {
+      return localize('Starting with "{start}" is not allowed', {'start': forbidden_start});
+    }
+
+    return true;
+}
+
+function standard_check_does_not_end_with(value, forbidden_end) {
+    if (value && value[value.length - 1] == forbidden_end) {
+      return localize('Ending with "{end}" is not allowed', {'end': forbidden_end});
+    }
+
+    return true;
+}
+
+
+
+
+// -- Standard validators ----------------------------------------------
+
+function standard_not_empty_validator(node) {
+    const value = node.value;
+
+    return standard_check_not_empty(value);
+}
+
 function standard_password_validator(node) {
     const password = node.value;
     const minimum_password_length = 6;
 
-    if (password.length < minimum_password_length) {
-        return localize('Too short (minimal length: {minimal_length})', {'minimal_length': String(minimum_password_length)});
-    }
-    return true;
+    return standard_check_minimal_length(password, minimum_password_length)
 }
 
 function standard_username_validator(node) {
   const username = node.value;
   if (!username.match(/^[a-z0-9][a-z0-9-]{2,37}[a-z0-9]$/)) {
-    if (username.length < 4) {
-      return localize('Too short (minimal length: {minimal_length})', {'minimal_length': '4'});
+    var result = standard_check_minimal_length(username, 4);
+
+    if (result === true) {
+        result = standard_check_maximal_length(username, 39);
     }
 
-    if (username[0] == '-') {
-      return localize('Starting with "{start}" is not allowed', {'start': username[0]});
+    if (result === true) {
+        result = standard_check_does_not_start_with(username, '-');
     }
 
-    if (username[username.length - 1] == '-') {
-      return localize('Ending with "{end}" is not allowed', {'end': username[username.length - 1]});
+    if (result === true) {
+        result = standard_check_does_not_end_with(username, '-');
     }
 
-    if (username.length > 39) {
-      return localize('Too long (maximum length: {maximum_length})', {'maximum_length': '39'});
+    if (result !== true) {
+        return result;
     }
 
     return localize('Only lowercase letters, digits and dashes are allowed');
