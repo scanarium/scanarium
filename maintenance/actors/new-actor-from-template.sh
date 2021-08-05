bc4a7a62f5887914f73dbbc0ae10ed4876c9683d
Christian Aistleitner <christian@quelltextlich.at>
2021-08-05 08:07:22 +0200

    maintenance/actors: Add script to start new actor as copy of old actor

---
+++ maintenance/actors/new-actor-from-template.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# This file is part of Scanarium https://scanarium.com/ and licensed under the
+# GNU Affero General Public License v3.0 (See LICENSE.md)
+# SPDX-License-Identifier: AGPL-3.0-only
+
+set -e
+set -o pipefail
+
+# Call as:
+#
+#  maintenance/actors/new-actor-from-template.sh scene/$SCENE/actors/$OLD_NAME $NEW_NAME
+#
+
+cd "$(dirname "$0")"
+cd ../..
+
+TEMPLATE="$1"
+NEW_NAME="$2"
+
+SCENE_DIR="scenes"
+ACTORS_DIR="actors"
+
+OLD_NAME="$(basename "$TEMPLATE")"
+SCENE="$(basename "$(dirname "$(dirname "$TEMPLATE")")")"
+
+FULL_TARGET_DIR="$SCENE_DIR/$SCENE/$ACTORS_DIR/$NEW_NAME"
+
+update_qr_map() {
+    NEXT_ID=$(($(grep '"[0-9][0-9]*":' conf/qr-code-maps/SCANARIUM.COM.json | sort | tail -n 1 | sed -e 's/^ *"0*\([1-9][0-9]*\)\":.*/\1/') + 1))
+    NEXT_ID_FORMATTED=$(printf "%03d" "$NEXT_ID")
+    sed -e 's/^\( *"001":.*\)/"'"$(printf "%03d" "$NEXT_ID")"'":"'"$SCENE:$NEW_NAME"'",\n\1/' -i conf/qr-code-maps/SCANARIUM.COM.json
+    maintenance/qr-code-maps/sort.sh
+}
+
+create_actor_files() {
+    mkdir -p "$FULL_TARGET_DIR"
+
+    for VARIANT in \
+        ".js" \
+        "-undecorated.svg" \
+
+    do
+        cp "$TEMPLATE/$OLD_NAME$VARIANT" "$FULL_TARGET_DIR/$NEW_NAME$VARIANT"
+    done
+    sed -e "s/$OLD_NAME/$NEW_NAME/g" -i "$FULL_TARGET_DIR/$NEW_NAME.js"
+}
+
+create_actor() {
+    echo "$TEMPLATE -> $NEW_NAME"
+
+    create_actor_files
+    update_qr_map
+}
+
+create_actor
+
+echo "pass"
